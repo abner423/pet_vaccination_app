7642fbcd179ee465d59797079546b2371037ae76
byronkamal <byronkamal@hotmail.com>
2020-06-18 23:19:27 -0300

    Mudando tematica para vacinação de animais

---
+++ lib/utils/pet_icons.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class Pets {
+  Pets._();
+
+  static const _kFontFam = 'Pets';
+
+  static const IconData cat = const IconData(0xe800, fontFamily: _kFontFam);
+  static const IconData dog_seating =
+      const IconData(0xe801, fontFamily: _kFontFam);
+}
